afa97d7765ea575b0864aacfc81053b3c955cf90
Joshua <55904573+Insomniquak@users.noreply.github.com>
2019-10-05 11:12:05 +0800

    changed files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let root = document.getElementById('root')
+let name = "joshua"
+let superman = {
+  name:"clarke kent",
+  age:37,
+  weight:85,
+}
+
+let DisplaySuperhero = (properties) => {
+  return ( 
+  <>
+    <p>Name: {properties.name}</p>
+    <p>Age: {properties.age}</p>
+    <p>weight:{properties.weight}</p>
+  </>
+  )
+}
+ReactDOM.render(<DisplaySuperhero
+  name={superman.name}
+  age={superman.age}
+  weight={superman.weight}
+  img={superman.img}
+
+  />,root);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// let jsx = (
+//   <div>
+//     { !name === "" ? <p>No Name</p> : <h1>{name}</h1> }
+//     <p>Hello world</p>
+//   </div>
+// )
+// let superhero = (
+//   <>
+//     <p>Name: {superman.name}</p>
+//     <p>Age: {superman.age}</p>
+//     <p>weight:{superman.weight}</p>
+//   </>
+// )
